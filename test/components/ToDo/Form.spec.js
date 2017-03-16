cf0cae17062214866f9af02586e777c66fd17f2d
Sergey Atyagin <elemus025@gmail.com>
2017-03-16 22:46:24 +0100

    Update Form.spec.js

--- test/components/ToDo/Form.spec.js
+++ test/components/ToDo/Form.spec.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
+import sinon from 'sinon';
 import Form from '../../../src/components/Todo/Form';
 
-const setup = () => {
-  const props = {
-    onTaskAdd: () => {},
+const setup = (props = { onTaskAdd() {}}) => {
+  const { onTaskAdd } = props;
+
+  const componentProps = {
+    onTaskAdd,
   };
 
-  return shallow(<Form {...props} />);
+  return shallow(<Form {...componentProps} />);
 };
 
 describe('Component | ToDo | Form', () => {
@@ -19,5 +22,23 @@ describe('Component | ToDo | Form', () => {
     expect(form.find('input')).to.have.lengthOf(1);
     expect(form.find('button').text()).to.equal('Add');
   });
-});
 
+  it('should change state on input event', () => {
+    const form = setup();
+    const description = 'test task';
+
+    form.find('input').simulate('input', { target: { value: description } });
+
+    expect(form.state().description).to.equal(description);
+  });
+
+  it('should call onTaskAdd() on form submit', () => {
+    const onTaskAdd = sinon.spy();
+    const form = setup({ onTaskAdd });
+
+    form.find('form').simulate('submit', { preventDefault() {} });
+
+    expect(onTaskAdd.calledOnce).to.equal(true);
+    expect(form.state().description).to.equal('');
+  });
+});
