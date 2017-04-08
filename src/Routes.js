023bb973a4f5d0ac055b40e949bdc287e41d2f03
Sergey Atyagin <elemus025@gmail.com>
2017-04-08 22:10:31 +0200

    Update dependencies: react, react-dom, react-addons-test-utils. Installed: prop-types

--- src/Routes.js
+++ src/Routes.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Router, Route } from 'react-router';
 
 import Todo from './containers/Todo';
