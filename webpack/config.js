c185d73db37e1af13ca6dd89ddd436b5e0745c7c
Sergey Atyagin <elemus025@gmail.com>
2017-05-20 20:36:51 +0200

    Store babel configuration in .babelrc

--- webpack/config.js
+++ webpack/config.js
@@ -5,35 +5,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const outputPath = path.resolve(__dirname, '../dist/');
 const jsBundleName = '[name].[hash:8].min.js';
 
-const babelLoaderOptions = {
-  babelrc: false,
-  presets: [
-    [
-      'es2015',
-      { modules: false },
-    ],
-    'react',
-  ],
-  plugins: [
-    'transform-object-assign',
-    'transform-runtime',
-  ],
-  env: {
-    production: {
-      plugins: [
-        'transform-react-remove-prop-types',
-        'transform-react-inline-elements',
-        'transform-react-constant-elements',
-      ]
-    },
-    development: {
-      plugins: [
-        'react-hot-loader/babel',
-      ],
-    },
-  },
-};
-
 /**
  * General webpack settings
  */
@@ -57,10 +28,7 @@ module.exports = {
         test: /\.jsx?$/,
         include: [path.resolve(__dirname, '../src')],
         exclude: [/node_modules/],
-        use: {
-          loader: 'babel-loader',
-          options: babelLoaderOptions,
-        },
+        loader: 'babel-loader',
       },
     ],
   },
